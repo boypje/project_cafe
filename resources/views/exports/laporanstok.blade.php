815ea4d060f78cd7ae8e1d0c83762533fd46c599
Tri Adi <triadilaksamana@gmail.com>
2023-10-17 14:41:09 +0700

    Fix: layout

--- resources/views/exports/laporanstok.blade.php
+++ resources/views/exports/laporanstok.blade.php
@@ -6,29 +6,45 @@
             <th>Total Transaksi</th>
             <th>Total Pengunjung</th>
             @foreach($products as $product)
-                <th>{{ $product->nama_produk }}</th>
+            <th>{{ $product->nama_produk }}</th>
             @endforeach
             <th>Total Terjual</th>
         </tr>
     </thead>
     <tbody>
-    @foreach ($data as $trx)
+        @foreach ($data as $trx)
         @if (is_array($trx))
-            @continue
-        @endif
+        <tr>
+            <td>{{ $trx['Tanggal'] }}</td>
+            <td>{{ $trx['kasir'] }}</td>
+            <td>{{ $trx['total_transaksi'] }}</td>
+            <td>{{ $trx['total_pengunjung'] }}</td>
+            @foreach ($products as $product)
+            @php
+            $columnName = 'total_produk_' . $product->id_produk . '_terjual';
+            @endphp
+            <td>{{ $trx[$columnName] }}</td>
+            @endforeach
+            <td>{{ $trx['total_semua_produk_terjual'] }}</td>
+        </tr>
+
+        @else
         <tr>
             <td>{{ $trx->Tanggal }}</td>
             <td>{{ $trx->kasir }}</td>
             <td>{{ $trx->total_transaksi }}</td>
             <td>{{ $trx->total_pengunjung }}</td>
             @foreach ($products as $product)
-                @php
-                    $columnName = 'total_produk_' . $product->id_produk . '_terjual';
-                @endphp
-                <td>{{ $trx->$columnName }}</td>
+            @php
+            $columnName = 'total_produk_' . $product->id_produk . '_terjual';
+            @endphp
+            <td>{{ $trx->$columnName }}</td>
             @endforeach
             <td>{{ $trx->total_semua_produk_terjual }}</td>
-        </tr>   
-    @endforeach 
+        </tr>
+        @endif
+
+
+        @endforeach
     </tbody>
 </table>
